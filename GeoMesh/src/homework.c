f2ad515357625e07d90916ab859711c8659cc0a1
Jackli77 <56607244+Jackli77@users.noreply.github.com>
2023-05-07 00:52:47 +0200

    a

--- GeoMesh/src/homework.c
+++ GeoMesh/src/homework.c
@@ -3,18 +3,18 @@
 
 
 
-double geoSize(double x, double y){
+double geoSize(double x, double y) {
 
     femGeo* theGeometry = geoGetGeometry();
-    
+
     double h = theGeometry->h;
     double x0 = theGeometry->xNotch;
     double y0 = theGeometry->yNotch;
     double r0 = theGeometry->rNotch;
     double h0 = theGeometry->hNotch;
     double d0 = theGeometry->dNotch;
-  
-    
+
+
     double x1 = theGeometry->xHole;
     double y1 = theGeometry->yHole;
     double r1 = theGeometry->rHole;
@@ -22,23 +22,29 @@ double geoSize(double x, double y){
     double d1 = theGeometry->dHole;
 
 
-//
-//     A modifier !
-//     
-// Your contribution starts here ....
-//
-    
-     
-    return h;
-    
-//   
-// Your contribution ends here :-)
-//
+
+    double hfinal = h;
+    double d = sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0)) - r0;
+    if (d < d0) {
+        double a = (-2 * h + 2 * h0) / (d0 * d0 * d0);
+        double b = (3 * h - 3 * h0) / (d0 * d0);
+        double c = 0;
+        hfinal = a * d * d * d + b * d * d + c * d + h0;
+    }
+
+    d = sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)) - r1;
+    if (d < d1) {
+        double a = (-2 * h + 2 * h1) / (d1 * d1 * d1);
+        double b = (3 * h - 3 * h1) / (d1 * d1);
+        double c = 0;
+        hfinal = fmin(hfinal, a * d * d * d + b * d * d + c * d + h1);
+    }
+
+    return hfinal;
+
 
 }
 
-
-#define ___ 0
 
 void geoMeshGenerate() {
 
@@ -46,67 +52,60 @@ void geoMeshGenerate() {
 
     double w = theGeometry->LxPlate;
     double h = theGeometry->LyPlate;
-     
+
     double x0 = theGeometry->xNotch;
     double y0 = theGeometry->yNotch;
     double r0 = theGeometry->rNotch;
-    
-    
+
+
     double x1 = theGeometry->xHole;
     double y1 = theGeometry->yHole;
     double r1 = theGeometry->rHole;
- 
-//
-//  -1- Construction de la géométrie avec OpenCascade
-//      On crée le rectangle
-//      On crée les deux cercles
-//      On soustrait les cercles du rectangle :-)
-//
- 
+
+    //
+    //  -1- Construction de la géométrie avec OpenCascade
+    //      On crée le rectangle
+    //      On crée les deux cercles
+    //      On soustrait les cercles du rectangle :-)
+    //
+
     int ierr;
-    int idPlate = gmshModelOccAddRectangle(___, ___, ___, ___, ___, ___, ___,&ierr);   
-    ErrorGmsh(ierr);
-    int idNotch = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr); 
-    ErrorGmsh(ierr);
-    int idHole  = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr); 
-    ErrorGmsh(ierr);
-    
-    int plate[] = {___,___};
-    int notch[] = {___,___};
-    int hole[]  = {___,___};
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    ErrorGmsh(ierr);
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    ErrorGmsh(ierr);
- 
-//
-//  -2- Définition de la fonction callback pour la taille de référence
-//      Synchronisation de OpenCascade avec gmsh
-//      Génération du maillage (avec l'option Mesh.SaveAll :-)
-                  
-   
+    int idPlate = gmshModelOccAddRectangle(-w / 2.0, -h / 2.0, 0.0, w, h, -1, 0.0, &ierr);
+    int idNotch = gmshModelOccAddDisk(x0, y0, 0.0, r0, r0, -1, NULL, 0, NULL, 0, &ierr);
+    int idHole = gmshModelOccAddDisk(x1, y1, 0.0, r1, r1, -1, NULL, 0, NULL, 0, &ierr);
+
+    int plate[] = { 2,idPlate };
+    int notch[] = { 2,idNotch };
+    int hole[] = { 2,idHole };
+    gmshModelOccCut(plate, 2, notch, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr);
+    gmshModelOccCut(plate, 2, hole, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr);
+
+    //
+    //  -2- Définition de la fonction callback pour la taille de référence
+    //      Synchronisation de OpenCascade avec gmsh
+    //      Génération du maillage (avec l'option Mesh.SaveAll :-)
+
     geoSetSizeCallback(geoSize);
-                                  
-    gmshModelOccSynchronize(&ierr);       
+    gmshModelOccSynchronize(&ierr);
     gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
-    gmshModelMeshGenerate(2, &ierr);  
-       
-//
-//  Generation de quads :-)
-//
-//    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
-//    gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
-//    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  chk(ierr);
-//    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  chk(ierr);
-//    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  chk(ierr);
-//    gmshModelMeshGenerate(2, &ierr);  
-   
- 
-//
-//  Plot of Fltk
-//
-//   gmshFltkInitialize(&ierr);
-//   gmshFltkRun(&ierr);  chk(ierr);
-//
-    
+    gmshModelMeshGenerate(2, &ierr);
+
+    //
+    //  Generation de quads (avec quelques triangles...) :-)
+    //
+    //    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+    //    gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
+    //    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  chk(ierr);
+    //    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  chk(ierr);
+    //    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  chk(ierr);
+    //    gmshModelMeshGenerate(2, &ierr);  
+
+
+    //
+    //  Plot avec Fltk et vous avez accès au graphique de gmsh :-)
+    //
+    //   gmshFltkInitialize(&ierr);
+    //   gmshFltkRun(&ierr);  chk(ierr);
+    //
+
 }
--- LinearElasticity/src/homework.c
+++ LinearElasticity/src/homework.c
@@ -9,72 +9,120 @@ void geoMeshGenerate() {
 
     double w = theGeometry->LxPlate;
     double h = theGeometry->LyPlate;
-    
-    int ierr;
-    double r = w/4;
-    int idRect = gmshModelOccAddRectangle(0.0,0.0,0.0,w,h,-1,0.0,&ierr); 
-    int idDisk = gmshModelOccAddDisk(w/2.0,h/2.0,0.0,r,r,-1,NULL,0,NULL,0,&ierr); 
-    int idSlit = gmshModelOccAddRectangle(w/2.0,h/2.0-r,0.0,w,2.0*r,-1,0.0,&ierr); 
-    int rect[] = {2,idRect};
-    int disk[] = {2,idDisk};
-    int slit[] = {2,idSlit};
 
-    gmshModelOccCut(rect,2,disk,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    gmshModelOccCut(rect,2,slit,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    gmshModelOccSynchronize(&ierr); 
+    int ierr;
+    double r = w / 4;
+    int idRect = gmshModelOccAddRectangle(0.0, 0.0, 0.0, w, h, -1, 0.0, &ierr);
+    int idDisk = gmshModelOccAddDisk(w / 2.0, h / 2.0, 0.0, r, r, -1, NULL, 0, NULL, 0, &ierr);
+    int idSlit = gmshModelOccAddRectangle(w / 2.0, h / 2.0 - r, 0.0, w, 2.0 * r, -1, 0.0, &ierr);
+    int rect[] = { 2,idRect };
+    int disk[] = { 2,idDisk };
+    int slit[] = { 2,idSlit };
+
+    gmshModelOccCut(rect, 2, disk, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr);
+    gmshModelOccCut(rect, 2, slit, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr);
+    gmshModelOccSynchronize(&ierr);
 
     if (theGeometry->elementType == FEM_QUAD) {
-        gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
-        gmshOptionSetNumber("Mesh.RecombineAll",1,&ierr);
-        gmshOptionSetNumber("Mesh.Algorithm",8,&ierr);  
-        gmshOptionSetNumber("Mesh.RecombinationAlgorithm",1.0,&ierr); 
-        gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  
-        gmshModelMeshGenerate(2,&ierr);  }
-  
+        gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+        gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
+        gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);
+        gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);
+        gmshModelGeoMeshSetRecombine(2, 1, 45, &ierr);
+        gmshModelMeshGenerate(2, &ierr);
+    }
+
     if (theGeometry->elementType == FEM_TRIANGLE) {
-        gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
-        gmshModelMeshGenerate(2,&ierr);  }
- 
+        gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+        gmshModelMeshGenerate(2, &ierr);
+    }
+
     return;
 }
 
 
-double *femElasticitySolve(femProblem *theProblem)
+double* femElasticitySolve(femProblem* theProblem)
 {
 
-    femFullSystem  *theSystem = theProblem->system;
-    femIntegration *theRule = theProblem->rule;
-    femDiscrete    *theSpace = theProblem->space;
-    femGeo         *theGeometry = theProblem->geometry;
-    femNodes       *theNodes = theGeometry->theNodes;
-    femMesh        *theMesh = theGeometry->theElements;
-    
-    
-    double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
-    int iElem,iInteg,iEdge,i,j,d,map[4],mapX[4],mapY[4];
-    
+    femFullSystem* theSystem = theProblem->system;
+    femIntegration* theRule = theProblem->rule;
+    femDiscrete* theSpace = theProblem->space;
+    femGeo* theGeometry = theProblem->geometry;
+    femNodes* theNodes = theGeometry->theNodes;
+    femMesh* theMesh = theGeometry->theElements;
+
+
+    double x[4], y[4], phi[4], dphidxsi[4], dphideta[4], dphidx[4], dphidy[4];
+    int iElem, iInteg, iEdge, i, j, d, map[4], mapX[4], mapY[4];
+
     int nLocal = theMesh->nLocalNode;
 
-    double a   = theProblem->A;
-    double b   = theProblem->B;
-    double c   = theProblem->C;      
+    double a = theProblem->A;
+    double b = theProblem->B;
+    double c = theProblem->C;
     double rho = theProblem->rho;
-    double g   = theProblem->g;
-    double **A = theSystem->A;
-    double *B  = theSystem->B;
-    
-    
-  //
-  //  A faire :-)
-  //                
-                
-                
-  
-    int *theConstrainedNodes = theProblem->constrainedNodes;     
-    for (int i=0; i < theSystem->size; i++) {
+    double g = theProblem->g;
+    double** A = theSystem->A;
+    double* B = theSystem->B;
+
+
+    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
+        for (j = 0; j < nLocal; j++) {
+            map[j] = theMesh->elem[iElem * nLocal + j];
+            mapX[j] = 2 * map[j];
+            mapY[j] = 2 * map[j] + 1;
+            x[j] = theNodes->X[map[j]];
+            y[j] = theNodes->Y[map[j]];
+        }
+
+        for (iInteg = 0; iInteg < theRule->n; iInteg++) {
+            double xsi = theRule->xsi[iInteg];
+            double eta = theRule->eta[iInteg];
+            double weight = theRule->weight[iInteg];
+            femDiscretePhi2(theSpace, xsi, eta, phi);
+            femDiscreteDphi2(theSpace, xsi, eta, dphidxsi, dphideta);
+
+            double dxdxsi = 0.0;
+            double dxdeta = 0.0;
+            double dydxsi = 0.0;
+            double dydeta = 0.0;
+            for (i = 0; i < theSpace->n; i++) {
+                dxdxsi += x[i] * dphidxsi[i];
+                dxdeta += x[i] * dphideta[i];
+                dydxsi += y[i] * dphidxsi[i];
+                dydeta += y[i] * dphideta[i];
+            }
+            double jac = fabs(dxdxsi * dydeta - dxdeta * dydxsi);
+
+            for (i = 0; i < theSpace->n; i++) {
+                dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / jac;
+                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac;
+            }
+            for (i = 0; i < theSpace->n; i++) {
+                for (j = 0; j < theSpace->n; j++) {
+                    A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] +
+                        dphidy[i] * c * dphidy[j]) * jac * weight;
+                    A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] +
+                        dphidy[i] * c * dphidx[j]) * jac * weight;
+                    A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] +
+                        dphidx[i] * c * dphidy[j]) * jac * weight;
+                    A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] +
+                        dphidx[i] * c * dphidx[j]) * jac * weight;
+                }
+            }
+            for (i = 0; i < theSpace->n; i++) {
+                B[mapY[i]] -= phi[i] * g * rho * jac * weight;
+            }
+        }
+    }
+
+    int* theConstrainedNodes = theProblem->constrainedNodes;
+    for (int i = 0; i < theSystem->size; i++) {
         if (theConstrainedNodes[i] != -1) {
             double value = theProblem->conditions[theConstrainedNodes[i]]->value;
-            femFullSystemConstrain(theSystem,i,value); }}
-                            
+            femFullSystemConstrain(theSystem, i, value);
+        }
+    }
+
     return femFullSystemEliminate(theSystem);
 }
